54a8ff989d91056a0ffb590cfd3e1f3de8634a3a
Soren <ssheffer@peckham.org>
2026-06-01 09:28:26 -0400

    Added more messages.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,24 @@
 const approvalMessages = [
-    "This looks good, thanks",
-    "This is approved",
-    "Go ahead and proceed"
+    "This looks good, thanks.",
+    "This is approved.",
+    "Go ahead and proceed.",
+    "This is accurate.",
+    "Make it so.",
+    "Aye aye.",
+    "Provided answers to questions. Approved."
 ];
 
 const rejectionMessages = [
-    "Edits attached",
+    "Edits attached.",
     "Can we discuss this over Teams?",
-    "See responses to comments attached"
+    "See responses to comments attached.",
+    "Check with Dave.",
+    "Who are you?"
 ];
 
 let currentMessageType;
 let score = 0;
+let lastMesg;
 const yippee = new Audio("https://www.myinstants.com/media/sounds/yippee-tbh.mp3");
 const bwop = new Audio("https://www.myinstants.com/media/sounds/spongebob-boowomp.mp3");
 
@@ -25,16 +32,23 @@ function pickRandom (array) {
 
 function generateMessage () {
     let newMesg = "Something borked";
-    if (Math.random() > 0.25) {
-        newMesg = pickRandom(approvalMessages);
-        currentMessageType = "approve";
-    } else {
-        newMesg = pickRandom(rejectionMessages);
-        currentMessageType = "reject";
+    while (true) {
+        if (Math.random() > 0.25) {
+            newMesg = pickRandom(approvalMessages);
+            currentMessageType = "approve";
+        } else {
+            newMesg = pickRandom(rejectionMessages);
+            currentMessageType = "reject";
+        }
+        if (lastMesg != newMesg) {
+            break;
+        }
     }
 
     document.getElementById("messageSpan").textContent = newMesg;
     document.getElementById("score").textContent = score;
+
+    lastMesg = newMesg;
 }
 
 function check (clickedOption) {
